155ae29059212adaa449df95b723229fc6f01a63
Mayank kumar singh <mayank.reactjs@gmail.com>
2022-07-30 14:57:36 +0530

    removed demo components..

--- src/components/notification/Notification2.jsx
+++
@@ -1,9 +0,0 @@
-import Notification3 from "./Notification3"
-export default function Notification2(props) {
-  return (
-   <>
-      <div>Notification <span>TWO</span> {props.cartValue1}</div>
-       <Notification3 cartValue2={props.cartValue1}/>
-   </>
-  )
-}
--- src/components/notification/Notification3.jsx
+++
@@ -1,6 +0,0 @@
-const Notification3=(props)=>{
-  return(<>
- <div>Notification <span>THREE</span> {props.cartValue2} </div> 
-  </>)
-}
-export default Notification3;
